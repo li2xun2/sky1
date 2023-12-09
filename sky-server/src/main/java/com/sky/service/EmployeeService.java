d921f8b50fd51f6e006cc3d5c090299065e2f4d0
2046203578@qq.com <2046203578@qq.com>
2023-12-09 15:08:45 +0800

    员工分页查询 和 新增员工

--- sky-server/src/main/java/com/sky/service/EmployeeService.java
+++ sky-server/src/main/java/com/sky/service/EmployeeService.java
@@ -1,7 +1,11 @@
 package com.sky.service;
 
+import com.github.pagehelper.Page;
+import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
+import com.sky.result.PageResult;
 
 public interface EmployeeService {
 
@@ -12,4 +16,12 @@ public interface EmployeeService {
      */
     Employee login(EmployeeLoginDTO employeeLoginDTO);
 
+    /**
+     * 新增员工
+     * @param employeeDTO
+     * @return
+     */
+    void save(EmployeeDTO employeeDTO);
+
+    PageResult queryPage(EmployeePageQueryDTO employeePageQueryDTO);
 }
